f9f196baf2b495bd2cd16d2e9e367ce89a7ecb02
springer2424 <springer058@gmail.com>
2026-03-11 00:12:20 +0200

    getreports aded weth query but othrazetiom prob

--- server/controler/reportControler.js
+++ server/controler/reportControler.js
@@ -60,19 +60,45 @@ const bufferToObject = async (req,res) => {
 export const createReporrtsFromCsv = async (req,res) => {
     try{
         const data = await bufferToObject(req,res)
-    //    const {category, urgency, message} = req.body;
        const newReports = data.map((report) =>({ userid:req.user._id,...report,sourceType:"csvFile",createdAt:new Date().toLocaleString()}));
-      
        const mongoConn = await getMongoDbConn()
        const reportCollection = mongoConn.collection("reports")
        const reports = await reportCollection.insertMany(newReports)
     res.status(200).json({msg:"seccess",data:null})
-       
+    }catch(arr){
+        res.status(500).json({msg:arr.message,data:null})
+    }
+}
+
+export const getReports = async (req,res) => {
+    let serchBy;
+    const {agentCode, category, urgency} = req.query;
+    if(agentCode){
+        serchBy = agentCode
+    }
+    if(category){
+        serchBy = category
+    }
+    if(urgency){
+        serchBy = urgency   
+    }
+    try{
+        const mongoConn = await getMongoDbConn()
+        const reportCollection = mongoConn.collection("reports")
+        if(agentCode){
+        serchBy = {agentCode:agentCode} 
+        }
+        if(category){
+        serchBy = {category:category} 
+        }
+        if(urgency){
+        serchBy = {urgency:urgency}  
+        }
+        const reportsToPrint = await reportCollection.find(serchBy ? serchBy :{}).toArray()
+        return res.status(200).json({msg:"seccess",data:reportsToPrint})
 
     }catch(arr){
         res.status(500).json({msg:arr.message,data:null})
-
-
     }
 }
 
--- server/routs/authRout.js
+++ server/routs/authRout.js
@@ -1,13 +1,14 @@
 import express from "express"
 import { createAgent, logIn } from "../controler/adminControl.js";
 import { authMidelwer, otheraz } from "../controler/authControl.js";
-import { createReporrt, createReporrtsFromCsv, upload } from "../controler/reportControler.js";
+import { createReporrt, createReporrtsFromCsv, getReports, upload } from "../controler/reportControler.js";
 
 const router = express.Router();
 
 router.post("/users",authMidelwer,otheraz(["admin"]),createAgent);
 router.post("/login",logIn);
 router.post("/reports",authMidelwer,otheraz(["admin","agent"]),createReporrt)
+router.get("/reports",authMidelwer,otheraz(["admin","agent"]),getReports)
 router.post("/reports/csv",authMidelwer,otheraz(["admin","agent"]),upload.single(["faile"]),createReporrtsFromCsv)
 
 
