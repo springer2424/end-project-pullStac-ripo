d495d7f630a40d7979a69e87d1d1f194348dd1a5
springer2424 <springer058@gmail.com>
2026-03-10 18:05:08 +0200

    jghlkdgd

--- server/controler/reportControler.js
+++ server/controler/reportControler.js
@@ -1,7 +1,55 @@
+import { getMongoDbConn } from "../db/dbConect";
+import multer, { memoryStorage } from "multer"
 export const createReporrt = async (req,res) => {
     try{
        const {category, urgency, message} = req.body;
+       const newReport = {
+        userid:req.user._id,
+        category,
+        urgency,
+        message,
+        imagePath : req.file ? src/uploads/req.file.filename : null,
+        sourceType:"manual",
+        createdAt:new Date().toLocaleString()
+       }
+       const mongoConn = await getMongoDbConn()
+       const reportCollection = mongoConn.collection("reports")
+       const report = reportCollection.insertOne(newReport)
        
+
+    }catch{
 
     }
 }
+
+
+
+
+
+
+
+
+export const multerMidelwer = (extention,storig) => {
+    return multer({storig,
+        fileFilter: (req, file, cb) => {
+            const validFaile = extention.some(ext => file.mimetype.endsWith(ext))
+            cb(null, validFaile)
+        },
+        limits: 1024**2*5
+    })
+}
+export const upload = multerMidelwer(["csv"],multer.memoryStorage())
+
+
+
+
+
+
+
+// /reports/csv POST
+// מטרה: קליטת קובץ CSV והמרתו לדיווחים
+// Admin או Agent של Bearer token :הרשאה
+// מה מקבל
+// Content-Type: multipart/form-data
+// File required: csvFile
+// פורמט מומלץ לעמודות: message ,urgency ,category
--- server/routs/authRout.js
+++ server/routs/authRout.js
@@ -1,12 +1,14 @@
 import express from "express"
 import { createAgent, logIn } from "../controler/adminControl.js";
 import { authMidelwer, otheraz } from "../controler/authControl.js";
+import { createReporrt, createReporrtsCsv } from "../controler/reportControler.js";
 
 const router = express.Router();
 
 router.post("/users",authMidelwer,otheraz(["admin"]),createAgent);
 router.post("/login",logIn);
 router.post("/reports",authMidelwer,otheraz(["admin","agent"]),createReporrt)
+router.post("/reports/csv",authMidelwer,otheraz(["admin","agent"]),createReporrtsCsv)
 
 
 export default router;
